e5a9d942cb2c1fed34dfabb45ba92511a0471217
Volodymyr Udovychenko <vosap2007@i.ua>
2021-03-20 00:49:21 +0200

    фиксит поиск фильмов <form>

--- src/views/MoviesViews.js
+++ src/views/MoviesViews.js
@@ -30,22 +30,35 @@ export default class MoviesView extends Component {
         this.setState({...response});
         console.log(this.state.moviesName);*/
 
-        loadMovie = () => {
+        loadMovie = (e) => {
+          e.preventDefault();
             fetch(`https://api.themoviedb.org/3/search/movie?api_key=0516dd3e6a153d51192e61dfe30410f4&language=en-US&page=1&include_adult=true&query=${this.state.moviesName}`)
             .then(res => res.json())
             .then(movies => this.setState({moviesParam: movies.results}),
       )
               }
           
-
+/*<form onSubmit={this.onSubmitForm} className={styles.form}>
+          <input
+            type="text"
+            autoComplete="false"
+            placeholder="Enter movie"
+            value={input}
+            onChange={this.onChangeInput}
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button}>
+            Search
+          </button>
+        </form>*/
 
 
   render() { 
     
      return (
          <>
-        <div className="SearchForm">
-   <button type="submit" onClick={this.loadMovie} className="SearchForm-button">
+        <form onSubmit={this.loadMovie} className="SearchForm">
+   <button type="submit" /*onClick={this.loadMovie}*/ className="SearchForm-button">
      <span className="SearchForm-button-label">Search</span>
    </button>
 
@@ -56,7 +69,7 @@ export default class MoviesView extends Component {
      onChange={this.handleNameChange}
      placeholder="Search movies"
    />
- </div>
+ </form>
  <div><ul>
       {this.state.moviesParam.map(movie => (
           <li key={movie.id}><Link to={`/movies/${movie.id}`}>
